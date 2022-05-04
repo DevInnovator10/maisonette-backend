f38e0b7b39dba73f2057052c47a58227da1f6e64
DevInnovator10 <devinnovator10@gmail.com>
2022-05-04 00:00:00 -0400

    chore: update refused organizer spec

---
+++ spec/interactors/mirakl/order_state_machine/refused_organizer_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Mirakl::OrderStateMachine::RefusedOrganizer, mirakl: true do
+    it { expect(described_class.new).to be_a Interactor::Organizer }
+  it do
+    expect(described_class.organized).to(
+      eq [Mirakl::OrderStateMachine::ProcessOrderLineUpdateInteractor,
+          Mirakl::OrderStateMachine::SendOrderRejectionCancelationEmailInteractor,
+          Mirakl::BuildOrderFeePayloadInteractor,
+
+          Mirakl::SubmitOrderAdditionalFieldsInteractor]
+    )
+  end
+end
