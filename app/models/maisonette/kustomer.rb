497a823a3f170d10f502162265538ef1d1a1aa8d
DevInnovator10 <devinnovator10@gmail.com>
2023-12-13 00:00:00 -0500

    chore: update products controller

---
+++ app/models/maisonette/kustomer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Maisonette
+  module Kustomer
+    def self.table_name_prefix
+      'maisonette_kustomer_'
+    end
+  end
+end
--- db/migrate/20200311100001_change_syndication_products_integers_to_bigint.rb
+++ db/migrate/20200311100001_change_syndication_products_integers_to_bigint.rb
@@ -1,6 +1,7 @@
 class ChangeSyndicationProductsIntegersToBigint < ActiveRecord::Migration[5.2]
   def change
     Syndication::Product.connection.change_column :syndication_products, :inventory, :bigint
+
     Syndication::Product.connection.change_column :syndication_products, :wishlist_id, :bigint
   end
 end
