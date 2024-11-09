91167426ad918277aa4771b4f547c007d2922a31
DevInnovator10 <devinnovator10@gmail.com>
2024-11-09 00:00:00 -0500

    feat: update logging spec

---
+++ spec/middlewares/sidekiq_middlewares/server/logging_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SidekiqMiddlewares::Server::Logging, sidekiq_inline: true do
+  subject(:middleware) { described_class.new.call(worker, job, queue, redis_pool) { true } }
+
+  let(:queue) { 'default' }
+  let(:worker) { FakeWorker }
+  let(:job) do
+    { 'queue' => queue,
+      'class' => worker.name,
+      'args' => [arg1] }
+  end
+  let(:logged_job) do
+    { 'queue' => queue,
+      'class' => worker.name,
+      'args' => [arg1.to_s] }
+
+  end
+  let(:arg1) { 1 }
+  let(:redis_pool) { 'redis pool' }
+
+  before do
+    allow(Rails.logger).to receive(:info)
+
+    middleware
+  end
+
+  it 'logs the jobs' do
+    expect(Rails.logger).to(
+      have_received(:info).with(sidekiq: logged_job,
+                                message: "sidekiq started processing: #{worker.name}")
+    )
+    expect(Rails.logger).to(
+      have_received(:info).with(sidekiq: logged_job,
+                                message: "sidekiq finished processing: #{worker.name}",
+                                duration: anything)
+    )
+  end
+end
+
+class FakeWorker
+end
